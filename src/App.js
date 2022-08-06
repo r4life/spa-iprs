f5b3542f1e798fa45bdd7084aba3372fe94b748d
Sean Choi <r4life@gmail.com>
2022-08-06 18:45:42 +0100

    added video background

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import Nav from './components/Nav.js';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+
+import vid from './assets/vid01.mp4';
+import './index.css';
 
 const app = () => {
+
   return (
-    <div>app</div>
+    <div class="main">
+      <div class="overlay"></div>
+      <video src={vid} autoPlay loop muted />
+      <div class="content">
+        <h1>Get Fit Now</h1>
+        <br/>
+        <p>¨Some people want it to happen, some wish it would happen, others make it happen.¨ – Michael Jordan</p>
+      </div>
+
+    </div>
+
   )
 }
 
---
+++ src/components/Nav.js
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import CssBaseline from '@mui/material/CssBaseline';
+import useScrollTrigger from '@mui/material/useScrollTrigger';
+import Slide from '@mui/material/Slide';
+
+function HideOnScroll(props: Props) {
+  const { children, window } = props;
+  // Note that you normally won't need to set the window ref as useScrollTrigger
+  // will default to window.
+  // This is only being set here because the demo is in an iframe.
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+  });
+
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
+
+export default function HideAppBar(props: Props) {
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <HideOnScroll {...props}>
+        <AppBar>
+          <Toolbar>
+            <Typography variant="h6" component="div">
+              Scroll to hide App bar
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </HideOnScroll>
+      <Toolbar />
+    </React.Fragment>
+  );
+}
